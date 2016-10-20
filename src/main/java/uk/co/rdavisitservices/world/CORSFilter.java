2b47dad26385c87b966d1468f941e9b5c46825ed
Richard Davis <rich@davisfamily.eu>
2016-10-20 12:37:11 +0100

    CORS and Logging changes

--- src/main/java/uk/co/rdavisitservices/world/CORSFilter.java
+++ src/main/java/uk/co/rdavisitservices/world/CORSFilter.java
@@ -8,11 +8,13 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Component;
 
 @Component
-public class MyFilter implements Filter {
+public class CORSFilter implements Filter {
 
   @Override
   public void doFilter(ServletRequest req, ServletResponse res,
@@ -24,6 +26,19 @@ public class MyFilter implements Filter {
 	  if (delayStr != null){
 		  delaySeconds(Integer.parseInt(delayStr));
 	  }
+	  
+	  // add CORS support as embedded Tomcat does not provide it
+	  // CORS enabled browsers will not allow access without this block
+	  HttpServletRequest request = (HttpServletRequest) req;
+	  HttpServletResponse response = (HttpServletResponse) res;
+
+	  response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
+	  response.setHeader("Access-Control-Allow-Credentials", "true");
+	  response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
+	  response.setHeader("Access-Control-Max-Age", "3600");
+	  response.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With, remember-me");
+	  // End of CORS
+	  
 	  chain.doFilter(req, res);
   }
 
